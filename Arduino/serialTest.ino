a6af26471cda2df4b9e996147eb8e91a54ac9385
fatlard1993 <chase.whitfield@me.com>
2015-04-01 23:14:39 -0700

    Update serialTest.ino

--- Arduino/serialTest.ino
+++ Arduino/serialTest.ino
@@ -1,24 +1,44 @@
 String writeData = "nigga";
-bool isConnected = false;
+bool isConnected;
+bool responded;
+
+String info = "{'id': 1}";
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Im #1");
+  isConnected = false;
+  responded = false;
+  delay(100);
+  Serial.println("Im a module!");
 }
 
 void loop() {
-  if(isConnected){
-    delay(1000);
+  if(writeData != "nigga"){responded = true;}
+  if(isConnected && random(0, 19999) == 1 && !responded){
     Serial.println(writeData);
   }
 }
 
 void serialEvent() {
   String serialRecieved = Serial.readStringUntil('\n');
-  Serial.println(serialRecieved);
   if(serialRecieved == "good"){
     isConnected = true;
+    Serial.println("connected");
   } else if(serialRecieved == "stop"){
-    writeData = "cracker!";
+    switch(random(0, 4)){
+      case 0:
+        writeData = "cracker!";
+        break;
+      case 1:
+        writeData = "fine!";
+        break;
+      case 2:
+        writeData = "hoe!";
+        break;
+      case 3:
+        writeData = "ok..";
+        break;
+    }
+    Serial.println(writeData);
   }
 }
